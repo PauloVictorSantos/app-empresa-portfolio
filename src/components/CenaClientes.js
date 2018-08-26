fbb4e724f9bea68e5ad008e6df8ed3df209039ba
paulovictorln <paulovictor494@gmail.com>
2018-08-26 08:27:23 -0300

    app6

---
+++ src/components/CenaClientes.js
@@ -0,0 +1,62 @@
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    StatusBar,
+    Image,
+} from 'react-native';
+
+import BarraNavegacao from './BarraNavegacao';
+
+
+const detalheClientes = require('../imgs/detalhe_cliente.png');
+const cliente1 = require('../imgs/cliente1.png');
+const cliente2 = require('../imgs/cliente2.png');
+
+
+export default class CenaClientes extends Component {
+    render() {
+        return (
+            <View>
+                <StatusBar
+                    backgroundColor='#CCC'
+                />
+                <BarraNavegacao />
+                <View style={styles.cabecalho}>
+                    <Image source={detalheClientes} />
+                    <Text style={styles.textTitulo}>Nossos Clientes</Text>
+                </View>
+                <View style={styles.detalheCliente}>
+                    <Image source={cliente1} />
+                    <Text style={styles.txtDetalheCliente}>loren impsasdabsd asdkjashdsa</Text>
+                </View>
+                <View style={styles.detalheCliente}>
+                    <Image source={cliente2} />
+                    <Text style={styles.txtDetalheCliente}>loren impsasdabsd asdkjashdsdfsdf</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    cabecalho: {
+        flexDirection: 'row',
+        marginTop: 20
+    },
+    textTitulo: {
+        fontSize: 30,
+        color: '#b9c941',
+        marginTop: 25
+    },
+    detalheCliente:{
+        padding: 20,
+        marginTop: 10
+    },
+    txtDetalheCliente:{
+        fontSize: 18,
+        marginLeft: 20
+    }
+});
---
+++ src/components/CenaPrincipal.js
@@ -0,0 +1,65 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  TouchableHighlight
+} from 'react-native';
+
+import BarraNavegacao from './BarraNavegacao';
+
+const logo = require('../imgs/logo.png');
+const MenuEmpresa = require('../imgs/menu_cliente.png');
+const MenuServico = require('../imgs/menu_servico.png');
+const MenuContato = require('../imgs/menu_contato.png');
+const MenuCliente = require('../imgs/menu_cliente.png');
+
+
+export default class CenaPrincipal extends Component {
+  render() {
+    return (
+      <View>
+        <StatusBar
+          backgroundColor='#CCC'
+        />
+        <BarraNavegacao />
+        <View style={styles.logo}>
+          <Image source={logo} />
+        </View>
+        <View style={styles.menu}>
+          <View style={styles.menuGrupo}>
+            <TouchableHighlight onPress={()=> this.props.navigator.push({id: 'b'})}>
+              <Image source={MenuEmpresa} style={styles.imgMenu} />
+            </TouchableHighlight>
+            <Image source={MenuServico} style={styles.imgMenu} />
+          </View>
+          <View style={styles.menuGrupo}>
+            <Image source={MenuContato} style={styles.imgMenu} />
+            <Image source={MenuCliente} style={styles.imgMenu} />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  logo: {
+    marginTop: 30,
+    alignItems: 'center'
+  },
+  menu: {
+    alignItems: 'center'
+  },
+  menuGrupo: {
+    flexDirection: 'row'
+  },
+  imgMenu: {
+    margin: 15
+  }
+
+});
